b35d8394b62fb0fcb8a023ed5e98e8731d5776ad
Simon Ho <simon@strongloop.com>
2015-06-17 20:44:12 -0700

    Externalize client

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 var express = require('express');
-var request = require('request');
 
 var app = express();
 
 app.get('/', function(req, res) {
-  setTimeout(startLeaking, 10000);
+  function LeakyClass() {}
 
-  function startLeaking() {
-    function LeakyClass() {}
-
-    setInterval(function() {
-      var leaks = [];
-      for (var i = 0; i < 1000; i++)
-        leaks.push(new LeakyClass());
-      leaks = [];
-      console.log(leaks.length);
-      // spikeCpu();
-    }, 1000);
-  }
-
-  res.end('done');
+  var leaks = [];
+  for (var i = 0; i < 1000; i++)
+    leaks.push(new LeakyClass());
+  leaks = [];
+  console.log(leaks.length);
+  res.end('.');
 });
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log('Listening on ', port);
-});
-
-request('http://localhost:' + port, function(err, res, body) {
-  console.log(body);
+  console.log('Listening on', port);
 });
