b5426e331c7035861c2e5e1a22d4b2886eef5832
Setogit <setogit@gmail.com>
2015-06-17 23:48:47 -0700

    leak using large strings

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 var express = require('express');
+var request = require('request');
 
 var app = express();
 
-app.get('/', function(req, res) {
-  function LeakyClass() {}
+var STR = "ssadfsd;flk alsdfkj saldfkjsd flaskdjf \
+asldkjf saldkjfasldkfj asdlkfjas ;dlfkjsa df;lkasdjfklj \
+f;lkasdjf asldkfjas slkjf sasdfsdfsadf asdfsdf \
+asdfasdfasdfsadfasdfwesad fsdfskadfj aslkdjhjkf ksaldjhf  \
+slkadfj salkdjf ;alskdfj asl;dkfj asl;dkfja s;lkdjf sl \
+saldfkjs;dlf j;aslkdfj sdfas";
 
+app.get('/', function(req, res) {
   var leaks = [];
-  for (var i = 0; i < 1000; i++)
-    leaks.push(new LeakyClass());
-  leaks = [];
+  for (var i = 0; i < 10000000; i++)
+    leaks.push(STR.slice());
   console.log(leaks.length);
   res.end('.');
 });
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 3001;
 app.listen(port, function() {
   console.log('Listening on', port);
 });
