2c9f904ce15c45d3b21f9fbd2db6e48d8d8bd369
Simon Ho <simon@strongloop.com>
2015-06-17 20:27:10 -0700

    Modify source to use HTTP

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-require('./subroutine')();
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+app.get('/', function(req, res) {
+  setTimeout(startLeaking, 10000);
+
+  function startLeaking() {
+    function LeakyClass() {}
+
+    setInterval(function() {
+      var leaks = [];
+      for (var i = 0; i < 1000; i++)
+        leaks.push(new LeakyClass());
+      leaks = [];
+      console.log(leaks.length);
+      // spikeCpu();
+    }, 1000);
+  }
+
+  res.end('done');
+});
+
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log('Listening on ', port);
+});
+
+request('http://localhost:' + port, function(err, res, body) {
+  console.log(body);
+});
